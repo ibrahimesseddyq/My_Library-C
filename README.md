48b36769462e920b0ac8cc97f9c04a296d504f66
armanovxx <iebeat2018@outlook.com>
2022-01-21 22:37:57 +0100

    reupload my lib c

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# My_Library-C
+# My Library [Language C]
+I reuploaded the language c challenges which i did in Piscine of 1337
+and I'm trying to complete them .
---
+++ lib/rev_int_tab.c
@@ -0,0 +1,18 @@
+void	ft_sort_in_tab (int *tab, int size)	
+{
+	int	i;
+	int	temp[];
+	while (i < size-1)
+	{
+			if (tab[i] > tab[i+1])
+			{
+			temp[i] = tab[i];
+			tab[i] = tab[i + 1];
+			tab[i + 1] = temp[i];
+			
+			}
+			else if(tab[i] < tab[i+1])
+				continue;
+			i++;
+	}
+}
